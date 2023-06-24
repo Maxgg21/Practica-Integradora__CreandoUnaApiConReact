ee1e0a573c05c5707bcdbcef3c6d10290c33be08
Maxgg21 <lionel.gegena@gmail.com>
2023-06-24 14:40:30 -0300

    Subiendo componente de ContentWrapper y ContentRowTop

--- src/components/ContentWrapper.js
+++ src/components/ContentWrapper.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Footer from './Footer';
+import ContentRowTop from './ContentRowTop';
 
 
 export const ConterWrapper = () => {
     return (
         <div>
-            <div id='content'></div>
+            <div id='content'>
+                <ContentRowTop />    
+            </div>
             <Footer />
         </div> 
     )
